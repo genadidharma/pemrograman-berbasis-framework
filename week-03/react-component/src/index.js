dc48b5988268c2350fdbef93442ec627a4d81728
Genadi Dharma <genadislawa@gmail.com>
2022-02-28 22:04:56 +0700

    c - react lifecycle

--- week-03/react-component/src/index.js
+++ week-03/react-component/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './Test';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App/>,
+  <Test/>,
   document.getElementById('root')
 );
 
