193116cd89f66a8a2017be709daadcc78da18210
Genadi Dharma <genadislawa@gmail.com>
2022-03-09 08:53:49 +0700

    d - interaksi api dengan method post

--- week-04/react-api/src/container/Blog Post/BlogPost.jsx
+++ week-04/react-api/src/container/Blog Post/BlogPost.jsx
@@ -5,7 +5,13 @@ import './BlogPost.css'
 class BlogPost extends Component {
 
     state = {
-        listArtikel: []
+        listArtikel: [],
+        insertArtikel: {
+            userId: 1,
+            id: 1,
+            title: "",
+            body: ""
+        }
     }
 
     getData = () => {
@@ -25,6 +31,30 @@ class BlogPost extends Component {
             })
     }
 
+    handleFormArtikel = (event) => {
+        let formInsertArtikel = { ...this.state.insertArtikel }
+        let timestamp = new Date().getTime()
+        formInsertArtikel['id'] = timestamp
+        formInsertArtikel[event.target.name] = event.target.value
+        this.setState({
+            insertArtikel: formInsertArtikel
+        })
+    }
+
+    handleTambahArtikel = () => {
+        fetch('http://localhost:3001/posts', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state.insertArtikel)
+        })
+            .then((response) => {
+                this.getData()
+            })
+    }
+
     componentDidMount() {
         this.getData()
     }
@@ -32,9 +62,26 @@ class BlogPost extends Component {
     render() {
         return (
             <div className="post-artikel">
+                <div className="form pb-2 border-bottom">
+                    <div className="form-group-row">
+                        <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" name="title" id="title" onChange={this.handleFormArtikel} />
+                        </div>
+                    </div>
+                    <div className="form-group-row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea name="body" id="body" rows="3" onChange={this.handleFormArtikel}></textarea>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTambahArtikel}>Tambah</button>
+                </div>
+
                 <h2>Daftar Artikel</h2>
                 {
-                    this.state.listArtikel.map(artikel => {
+                    this.state.listArtikel.reverse().map(artikel => {
                         return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />
                     })
                 }
