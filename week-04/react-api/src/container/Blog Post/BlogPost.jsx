6a96d685b6602055efb4c2cf6e6cc73c2bebbd8a
Genadi Dharma <genadislawa@gmail.com>
2022-03-09 08:28:13 +0700

    c - interaksi api dengan method delete

--- week-04/react-api/src/container/Blog Post/BlogPost.jsx
+++ week-04/react-api/src/container/Blog Post/BlogPost.jsx
@@ -8,7 +8,7 @@ class BlogPost extends Component {
         listArtikel: []
     }
 
-    componentDidMount() {
+    getData = () => {
         fetch('http://localhost:3001/posts')
             .then(response => response.json())
             .then(result => {
@@ -18,13 +18,24 @@ class BlogPost extends Component {
             })
     }
 
+    handleHapusArtikel = (data) => {
+        fetch(`http://localhost:3001/posts/${data}`, { method: 'DELETE' })
+            .then(() => {
+                this.getData()
+            })
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
     render() {
         return (
             <div className="post-artikel">
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel => {
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body}/>
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />
                     })
                 }
             </div>
