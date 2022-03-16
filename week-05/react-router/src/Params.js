9be7fa900b40b42cd210a7ea4d7a8461e3d08a4d
Genadi Dharma <genadislawa@gmail.com>
2022-03-16 08:13:51 +0700

    a - 2.  routing with url parameters

---
+++ week-05/react-router/src/Params.js
@@ -0,0 +1,46 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useParams
+} from 'react-router-dom'
+
+function Params() {
+    return (
+        <Router>
+            <div>
+                <h2>Accounts</h2>
+                <ul>
+                    <li>
+                        <Link to="/netflix">Netflix</Link>
+                    </li>
+                    <li>
+                        <Link to="/gmail">Gmail</Link>
+                    </li>
+                    <li>
+                        <Link to="/yahoo">Yahoo</Link>
+                    </li>
+                    <li>
+                        <Link to="/amazon">Amazon</Link>
+                    </li>
+                </ul>
+                <hr />
+                <Routes>
+                    <Route path="/:id" element={<Child />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+function Child() {
+    let { id } = useParams()
+    return (
+        <div>
+            <h3>ID: {id}</h3>
+        </div>
+    )
+}
+
+export default Params;
--- week-05/react-router/src/index.js
+++ week-05/react-router/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Params from './Params';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Params/>
   </React.StrictMode>,
   document.getElementById('root')
 );
