280189b429f0b8a4c2bf5c97f479ed47287c3791
Genadi Dharma <genadislawa@gmail.com>
2022-04-05 19:18:54 +0700

    index.js for component calling added

--- week-07/react-hoc/src/index.js
+++ week-07/react-hoc/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GenericContainerDemo from './GenericContainer/GenericContainerDemo';
+import CustomFormDemo from './CustomForm/CustomFormDemo';
+import LoaderDemo from './LoadDemo/LoaderDemo';
+import RequireAuthDemo from './ProtectedRoutes/RequireAuthDemo';
+import RefsDemo from './RefsDemo/RefsDemo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <GenericContainerDemo/> */}
+    {/* <CustomFormDemo/> */}
+    {/* <LoaderDemo/> */}
+    {/* <RequireAuthDemo/> */}
+    <RefsDemo/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
