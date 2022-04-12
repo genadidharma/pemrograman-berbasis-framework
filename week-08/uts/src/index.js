c7fc4c24f060921e487620be1b1482fa32438433
Genadi Dharma <genadislawa@gmail.com>
2022-04-12 21:05:43 +0700

    router & redux added

--- week-08/uts/src/index.js
+++ week-08/uts/src/index.js
@@ -1,14 +1,18 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import store from './stores'
 
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 );
