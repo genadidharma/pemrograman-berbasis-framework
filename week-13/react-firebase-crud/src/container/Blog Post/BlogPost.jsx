ef5ee24537ea433ed4cdacfc82d13d211c3416d9
Genadi Dharma <genadislawa@gmail.com>
2022-05-25 09:31:54 +0800

    1 - firebase crud added

--- week-13/react-firebase-crud/src/container/Blog Post/BlogPost.jsx
+++ week-13/react-firebase-crud/src/container/Blog Post/BlogPost.jsx
@@ -1,53 +1,79 @@
+import { initializeApp } from "firebase/app";
 import React, { Component } from "react";
 import Post from "../Post";
-import API from '../../services'
 import './BlogPost.css'
+import firebaseConfig from "../../firebase/config";
+import { getDatabase, onValue, ref, set } from "firebase/database";
+
 
 class BlogPost extends Component {
 
-    state = {
-        listArtikel: [],
-        insertArtikel: {
-            userId: 1,
-            id: 1,
-            title: "",
-            body: ""
+    constructor(props){
+        super(props)
+        initializeApp(firebaseConfig);
+
+        this.state = {
+            listArtikel: []
         }
     }
 
     ambilDataDariServer = () => {
-        API.getNewBlog().then(result => {
-            this.setState({
-                listArtikel: result
-            })
+        const db = getDatabase()
+        const blogRef = ref(db, '/')
+
+        onValue(blogRef, (snapshot) => {
+            const state = snapshot.val()
+            this.setState(state)
         })
     }
 
-    handleHapusArtikel = (data) => {
-        API.deleteNewBlog(data).then(() => {
-            this.ambilDataDariServer()
+    handleHapusArtikel = (idArtikel) => {
+        const {listArtikel} = this.state
+        const newState = listArtikel.filter(data => {
+            return data.uid !== idArtikel
         })
+
+        this.setState({listArtikel: newState})
     }
 
-    handleFormArtikel = (event) => {
-        let formInsertArtikel = { ...this.state.insertArtikel }
-        let timestamp = new Date().getTime()
-        formInsertArtikel['id'] = timestamp
-        formInsertArtikel[event.target.name] = event.target.value
-        this.setState({
-            insertArtikel: formInsertArtikel
-        })
+    simpanDataKeServer = () => {
+        const db = getDatabase()
+        set(ref(db, '/'), this.state)
     }
 
     handleTambahArtikel = () => {
-        API.postNewBlog(this.state.insertArtikel)
-        .then(() => {
-            this.ambilDataDariServer()
-        })
+        let title = this.refs.judulArtikel.value
+        let body = this.refs.isiArtikel.value
+        let uid = this.refs.uid.value
+
+        if(uid && title && body){
+            const {listArtikel} = this.state
+            const indeksArtikel = listArtikel.findIndex(data => {
+                return data.uid === uid
+            })
+            listArtikel[indeksArtikel].title = title
+            listArtikel[indeksArtikel].body = body
+            this.setState({listArtikel})
+        }else if(title && body){
+            const uid = new Date().getTime().toString()
+            const {listArtikel} = this.state
+            listArtikel.push({uid, title, body})
+            this.setState({listArtikel})
+        }
+
+        this.refs.judulArtikel.value = ""
+        this.refs.isiArtikel.value = ""
+        this.refs.uid.value = ""
     }
 
     componentDidMount() {
         this.ambilDataDariServer()
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState !== this.state){
+            this.simpanDataKeServer()
+        }
     }
 
     render() {
@@ -57,23 +83,23 @@ class BlogPost extends Component {
                     <div className="form-group-row">
                         <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
                         <div className="col-sm-10">
-                            <input type="text" className="form-control" name="title" id="title" onChange={this.handleFormArtikel} />
+                            <input type="text" className="form-control" name="title" id="title" ref={'judulArtikel'}/>
                         </div>
                     </div>
                     <div className="form-group-row">
                         <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
                         <div className="col-sm-10">
-                            <textarea name="body" id="body" rows="3" onChange={this.handleFormArtikel}></textarea>
+                            <textarea name="body" id="body" rows="3" ref={'isiArtikel'}></textarea>
                         </div>
                     </div>
-
+                    <input type="hidden" name="uid" ref={'uid'} />
                     <button type="submit" className="btn btn-primary" onClick={this.handleTambahArtikel}>Tambah</button>
                 </div>
 
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel => {
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel} />
+                        return <Post key={artikel.uid} judul={artikel.title} isi={artikel.body} idArtikel={artikel.uid} hapusArtikel={this.handleHapusArtikel} />
                     })
                 }
             </div>
---
+++ week-13/react-firebase-crud/src/firebase/config.js
@@ -0,0 +1,14 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2WjQc8wkX4RDJn-LOc77Bl1ExBc3R5xU",
+  authDomain: "react-firebase-crud-ed74c.firebaseapp.com",
+  projectId: "react-firebase-crud-ed74c",
+  storageBucket: "react-firebase-crud-ed74c.appspot.com",
+  messagingSenderId: "447291573188",
+  appId: "1:447291573188:web:a9def183ee7186e2f9da91"
+};
+
+export default firebaseConfig;
